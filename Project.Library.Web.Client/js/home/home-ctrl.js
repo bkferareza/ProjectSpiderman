a192c7068fe60c81aeb2888c2fba9bf0be2323eb
Brian Kevin <bkferareza@gmail.com>
2018-11-22 11:54:44 +0800

    Added read me

--- Project.Library.Web.Client/js/home/home-ctrl.js
+++ Project.Library.Web.Client/js/home/home-ctrl.js
@@ -1,3 +1,4 @@
 ﻿homeModule.controller('homeController', ['$scope', 'homeFactory', function ($scope, homeFactory) {
+    $scope.title = 'This is from the home controller';
     console.log('Working home controller');
 }]);
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+Read how to Activate IIS on windows.
+Deploy Project.Library.Web.Client to IIS.
+If URL Rewrite is not available, install devtools\rewrite_amd64.
